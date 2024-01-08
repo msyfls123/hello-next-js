df9dd9deeddd9c32d9e576c3ce6695aa5566c031
msyfls123 <msyfls123@gmail.com>
2024-01-08 10:25:35 +0800

    Initial Commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        123456
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
