be511a25732b3d13ed4abaf32efb243513bca1db
msyfls123 <msyfls123@gmail.com>
2024-01-23 23:33:21 +0800

    feat: try more next features

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,4 @@
-import AppRouteRouteModule, { AppRouteHandlerFn } from 'next/dist/server/future/route-modules/app-route/module';
+import AppRouteRouteModule from 'next/dist/server/future/route-modules/app-route/module';
 import { NextResponse } from 'next/server';
 
 export const GET: AppRouteRouteModule['handle'] = async function GET(request, { params }) {
@@ -6,4 +6,12 @@ export const GET: AppRouteRouteModule['handle'] = async function GET(request, { 
     params,
     hello: 123,
   })
+}
+
+export const POST: AppRouteRouteModule['handle'] = async function POST(request) {
+  const payload = await request.json();
+  return NextResponse.json({
+    payload,
+    msg: 'I got your message!'
+  }, { status: 200 })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js! from pages</h1>
+  return <h1 className="text-3xl font-bold underline bg-red-400 px-10 py-20">Hello, Next.js! from pages</h1>
 }
